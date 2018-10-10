b7c885b0b00aeabdea5ae0a5d450610ad70ebd0c
nathan <nathanmersha@gmail.com>
2018-10-10 19:11:07 +0300

    Added sample config file.

---
+++ src/mergedConfig.js
@@ -0,0 +1,12 @@
+/**
+ * @author              Nathan M. Degineh - N8
+ * @name                Node Gen
+ * @module              mergedConfig.js
+ * @description         Merged configuration
+ * @kind                Configuration
+ * @copyright           September 2018 Node Gen
+ */
+
+module.exports = {
+    mergedConfig : null
+};
